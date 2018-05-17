de3d19469fcd1f5591c32a5c5f11e1723939675e
Marek Dědič <developer@dedic.eu>
2018-05-17 13:25:32 +0200

    Added global loss to targetprop

--- src/optimise/train.jl
+++ src/optimise/train.jl
@@ -45,11 +45,11 @@ function train!(loss, data, opt; cb = () -> ())
   end
 end
 
-function targettrain!(model, data, opt; cb = () -> ())
+function targettrain!(model, loss, data, opt; cb = () -> ())
   cb = runall(cb)
   opt = runall(opt)
   @progress for d in data
-	@interrupts target!(model(d[1]), d[2])
+	@interrupts target!(model, loss(model(d[1]), d[2]))
     opt()
     cb() == :stop && break
   end
