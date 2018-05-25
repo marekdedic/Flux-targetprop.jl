0ba4be5c6eda4562f91ff12a6a5acc55ac4f8fbf
Marek Dědič <developer@dedic.eu>
2018-05-25 14:02:20 +0200

    Removed TargetSoftmax

--- src/targetprop.jl
+++ src/targetprop.jl
@@ -51,36 +51,7 @@ function Base.show(io::IO, l::TargetDense)
 	print(io, ")");
 end
 
-# TargetSoftmax
-
-mutable struct TargetSoftmax{S, T, L}
-	dual_W::S
-	dual_b::T
-	loss::L
-	in::Array
-	out::Array
-end
-
-treelike(TargetSoftmax);
-
-function TargetSoftmax(dim::Integer, loss; initW = glorot_uniform, initb = zeros)::TargetSoftmax
-	return TargetSoftmax(param(initW(dim, dim)), param(initb(dim)), loss, Array{Float32, 0}(),Array{Float32, 0}());
-end
-
-function (a::TargetSoftmax)(x)
-	a.in = data(x);
-	a.out = softmax(a.in);
-	return a.out;
-end
-
 # targetprop
-
-function targetprop!(a::TargetSoftmax, target)
-	W, b, σ = a.dual_W, a.dual_b, identity;
-	dual(x) = σ.(W*data(x) .+ b);
-	back!(a.loss(dual(a.out), a.in))
-	return data(dual(target));
-end
 
 function targetprop!(a::TargetDense, target)
 	back!(a.loss(target, a.out));
@@ -111,14 +82,6 @@ function targettrain!(model, modelloss, data, opt; cb = () -> ())
 end
 
 # difftargetprop
-
-function difftargetprop!(a::TargetSoftmax, target)
-	target += data(a.out);
-	W, b, σ = a.dual_W, a.dual_b, identity;
-	dual(x) = σ.(W*data(x) .+ b);
-	back!(a.loss(dual(a.out), a.in))
-	return data(dual(target) - dual(a.out));
-end
 
 function difftargetprop!(a::TargetDense, target)
 	target += data(a.out);
